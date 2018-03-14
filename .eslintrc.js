37d3177aa06da0925d17be01575e6a2673b0ebb8
Trymunx <trymunx@gmail.com>
2018-03-14 21:10:04 +0000

    Add eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,51 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es6": true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "sourceType": "module"
+  },
+  "rules": {
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "double"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ],
+    "no-console": "off",
+    "no-undef": "error",
+    "no-unused-vars": [
+      "error",
+      {
+        "args": "none"
+      }
+    ],
+    "brace-style": [
+      "error",
+      "1tbs",
+      {
+        "allowSingleLine": true
+      }
+    ],
+    "curly": [
+      "warn",
+      "all"
+    ],
+    "eol-last": [
+      "error",
+      "always"
+    ]
+  }
+};
