13ff06234b6a4a0e2733a18e380bd57619131213
Alpvax <alpvax@netscape.net>
2018-03-21 08:35:22 +0000

    Fix code style

--- src/client/index.js
+++ src/client/index.js
@@ -1,14 +1,17 @@
 import EventEmitter from "events";
+import Scene from "./render/scene";
 import ServerWorker from "worker-loader!./serverWorker.js";
 
 export default class Client {
   constructor(canvasElement) {
     this.canvas = canvasElement;
     this.gameBus = new EventEmitter();
-    this.setScene(/*TODO: main menu scene*/)
+    this.gameBus.on("scene", (sceneData) => this.setScene(new Scene(sceneData)));
+    this.setScene(/*TODO: main menu scene*/);
   }
   setScene(scene) {
     this.scene = scene;
+    console.log("Scene: ", this.scene);
   }
   start() {
     let ctx = this.canvas.getContext("2d");
--- src/gameserver/index.js
+++ src/gameserver/index.js
@@ -1,5 +1,7 @@
 import EventEmitter from "events";
 
+let obj = {pos: {x:0, y:50}, move: {x: 3, y:0}};
+
 export default class GameServer {
   constructor(interval) {
     this.internal = self instanceof WorkerGlobalScope;
@@ -8,39 +10,44 @@ export default class GameServer {
     this.gameBus = new EventEmitter();
     this.running = false;
     this.worlds = new Map();
+    this.paused = false;
   }
   update() {
-    //TODO: run game updates
+    if(!this.paused) {
+      console.log("GameServer update");
+      //TODO: run game updates and return sceneData
+      obj.pos.x += obj.move.x;
+      if(obj.pos.x > 500) {
+        obj.pos.x = 0;
+      }
+      return obj;
+    }
   }
   start() {
     this.running = true;
     const skipTicks = 1000 / this.tps;
-    let nextTick = performance.now()
+    let nextTick = performance.now();
     const performLoop = () => {
       while(performance.now() > nextTick) {
-        this.update();
+        this.networkHandler.sendToClients("scene", {timeStamp: nextTick, skip: skipTicks, toRender: this.update()});
         nextTick += skipTicks;
       }
-    }
-    if(this.interval && this.interval > 0)
-    {
+    };
+    if(this.interval && this.interval > 0) {
       let intervalLoop = setInterval(() => {
         if(!this.running) {
           clearInterval(intervalLoop);
-        }
-        else {
-          performLoop()
+        } else {
+          performLoop();
         }
       }, this.interval);
-    }
-    else {
+    } else {
       while(this.running) {
         performLoop();
       }
     }
   }
-  stop()
-  {
+  stop() {
     this.running = false;
   }
   connect(playerData, observer) {
