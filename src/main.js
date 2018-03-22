c1b3e9cedeb97dff70206993513cd36cf6fba3cb
Alpvax <alpvax@gmail.com>
2018-03-22 14:05:04 +0000

    Add firebase

--- src/main.js
+++ src/main.js
@@ -15,7 +15,18 @@ new Vue({
 });
 
 //TODO:
+//Initialise firebase
+import "firebase/auth"; //Add Authentication to namespace
+import "firebase/firestore"; //Add Firestore to namespace
 import Client from "./client";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCtqc0NPUkzmjpBl2pCo8Jil3zVH_3I75s",
+  authDomain: "drykenspel.firebaseapp.com",
+  projectId: "drykenspel"
+});
+
+const db = firebase.firestore();
 
 const client = new Client(document.getElementById("game"));
 client.gameBus.on("connected", (data) => {
