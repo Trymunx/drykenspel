9d8b7b81fbaeaf65effc89ebed4b7d70db0f269a
Alpvax <alpvax@gmail.com>
2018-03-22 14:05:04 +0000

    Add firebase

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
+import * as firebase from "firebase/app"; //Import namespace
+import "firebase/auth"; //Add Authentication to namespace
+import "firebase/firestore"; //Add Firestore to namespace
 import Client from "./client";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCtqc0NPUkzmjpBl2pCo8Jil3zVH_3I75s",
+  authDomain: "drykenspel.firebaseapp.com",
+  projectId: "drykenspel"
+});
+
+const db = firebase.firestore();
 
 const client = new Client(document.getElementById("game"));
 client.gameBus.on("connected", (data) => {
