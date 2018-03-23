9910fa6644e02045c81beb772f98974102ec68b7
Alpvax <alpvax@gmail.com>
2018-03-23 17:39:18 +0000

    Add Client Login

--- src/main.js
+++ src/main.js
@@ -3,6 +3,25 @@ import "firebase/auth"; //Add Authentication to namespace
 import "firebase/firestore"; //Add Firestore to namespace
 import Client from "./client";
 
+(function setupCanvas() {
+  const gameCanvas = document.getElementById("game");
+  const ctx = gameCanvas.getContext("2d");
+  window.addEventListener("resize", resize);
+
+  function resize() {
+    gameCanvas.width = window.innerWidth;
+    gameCanvas.height = window.innerHeight;
+    draw();
+  }
+  resize();
+
+  function draw() {
+    console.log("Drawing the game");
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
+  }
+})();
+
 firebase.initializeApp({
   apiKey: "AIzaSyCtqc0NPUkzmjpBl2pCo8Jil3zVH_3I75s",
   authDomain: "drykenspel.firebaseapp.com",
@@ -14,15 +33,23 @@ document.getElementById("game").addEventListener("click", () => {
   firebase.auth().signInWithEmailAndPassword("alpvax@netscape.net", "password");
 });
 
+let client = null;
+
 firebase.auth().onAuthStateChanged((user) => {
   if(user) {
-    const client = new Client(document.getElementById("game"));
+    client = new Client(document.getElementById("game"));
     client.gameBus.on("connected", (data) => {
       console.log("Client connected to server: ", data);
     });
     client.start();
     client.newLocalServer();
   } else {
+    client.quit();
+    let canvas = document.getElementById("game");
+    let ctx = canvas.getContext("2d");
+    let size = canvas.height * 0.2;
+    ctx.font = size + "px Arial";
+    ctx.fillText("Log in to continue!",10, (canvas.height + size) / 2);
     //LOGGED OUT!
   }
 });
