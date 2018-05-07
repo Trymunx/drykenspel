5b016c2e0074e313d00db3d370a47d63d4849064
Trymunx <trymunx@gmail.com>
2018-05-07 21:42:46 +0100

    Update eslint and style

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,62 @@
 // https://eslint.org/docs/user-guide/configuring
 
 module.exports = {
-  root: true,
-  parserOptions: {
-    parser: 'babel-eslint'
+  "env": {
+    "browser": true,
+    "es6": true
   },
-  env: {
-    browser: true,
-  },
-  extends: [
-    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential', 
-    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-    'standard'
+  "extends": [
+    "eslint:recommended",
+    "plugin:vue/essential"
   ],
-  // required to lint *.vue files
+  "parserOptions": {
+    "sourceType": "module"
+  },
+  "globals": {
+    "firebase": false
+  },
+  "rules": {
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "double"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ],
+    "no-console": "off",
+    "no-undef": "error",
+    "no-unused-vars": [
+      "error",
+      {
+        "args": "none"
+      }
+    ],
+    "brace-style": [
+      "error",
+      "1tbs",
+      {
+        "allowSingleLine": true
+      }
+    ],
+    "curly": [
+      "warn",
+      "all"
+    ],
+    "eol-last": [
+      "error",
+      "always"
+    ]
+  },
   plugins: [
     'vue'
-  ],
-  // add your custom rules here
-  rules: {
-    // allow async-await
-    'generator-star-spacing': 'off',
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  }
-}
+  ]
+};
